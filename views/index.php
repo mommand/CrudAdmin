a6d75d5c3d46a385c4f33db1c2db7a7e232d01c7
Ziaullah <mommand.csf@gmail.com>
2016-08-30 21:40:52 +0430

    second commit

---
+++ views/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyAdmin</title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row" style="margin-top:60px;">
+      <div class="col-md-4 col-md-offset-4">
+        <form action="" method="post">
+        <fieldset>
+        <legend>Databaes Creaton From</legend>
+          <div class="form-group">
+            <label>Enter Your Database Name:</label>
+            <input type="text" name="dbname" class="form-control">
+          </div>
+          <div class="form-group">
+           <input type="submit"  value="Create" class="btn btn-primary">
+          </div>	
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
